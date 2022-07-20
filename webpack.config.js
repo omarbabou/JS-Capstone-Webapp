43274837c7faf6049454576df6860757b32e3840
omar babou <omarbabou@MacBook-Air-2.local>
2022-07-20 12:25:49 +0200

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+
   entry: './src/index.js',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 
   module: {
