1e9ae950e005a283564df3619aeb0565e3879538
omarbabou <obabou50@gmail.com>
2022-07-21 16:04:38 +0200

    Update js to get number of likes

--- src/script/api.js
+++ src/script/api.js
@@ -7,9 +7,3 @@ export const getMovieById = async (id) => {
   const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
   return response.json();
 };
-
-export const countMovies = (movies) => {
-  if (movies.length) {
-    document.querySelector('.count-movies').innerHTML = `${movies.length}`;
-  }
-};
